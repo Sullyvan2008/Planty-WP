1522db0b01a0938485fd5a4b53896961d259f4ab
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-01-11 20:23:46 +0100

    header & footer

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -6,3 +6,20 @@ function theme_enqueue_styles(){
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
+
+/* HOOK FILTER */ 
+
+add_filter( 'show_admin_bar', '__return_false' );
+
+function my_wp_nav_menu_args( $args = '' ) {
+    if( is_user_logged_in() ) {
+    // Logged in menu to display
+    $args['menu'] = 3;
+     
+    } else {
+    // Non-logged-in menu to display
+    $args['menu'] = 2;
+    }
+    return $args;
+    }
+    add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
